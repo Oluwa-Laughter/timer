b754230ba346b286b41d03981f1a89f45aa9a5dd
Oluwa-Laughter <ismakinde@gmail.com>
2025-05-09 10:18:11 +0100

    added Timer Component, add timer form

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
-import { useRef } from "react";
-import Input from "./components/Input";
-import Form, { FormHandleRef } from "./components/Form";
-import Button from "./components/Button";
+import Header from "./components/Header";
+import Timer from "./components/Timer";
+import AddTimer from "./components/AddTimer";
 
 function App() {
-  const customForm = useRef<FormHandleRef>(null);
-
-  function handleSave(data: unknown) {
-    const extractedData = data as { name: string; age: string };
-    console.log(extractedData);
-    customForm.current?.clear();
-  }
-
   return (
-    <main>
-      <Form onSave={handleSave} ref={customForm}>
-        <Input type="text" label="Name" id="name" />
-        <Input type="number" label="Age" id="age" />
-
-        <p>
-          <Button>Save</Button>
-        </p>
-      </Form>
-    </main>
+    <>
+      <Header />
+      <main>
+        <Timer />
+        <AddTimer />
+      </main>
+    </>
   );
 }
 
